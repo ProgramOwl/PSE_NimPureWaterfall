f6fe185d4e8e2f386ca3dd853d908cf3ff7793df
mbrannen747 <mbrannen@student.neumont.edu>
2018-01-12 13:48:32 -0700

    added all unit tests for main. Haven't checked functioning yet; will do that now

--- unitTests.js
+++ unitTests.js
@@ -111,50 +111,171 @@ function UTRowFourClickedRemovesAChild(){
         DisplayResult(false,"UTRowFourClickedRemovesAChild");
 }
 function UTEndTurnButtonWithComputerAndComputerWins(){
-
+    isPlayerVsComputer = true;
+    isPlayer1Turn = true;
+    RemoveAllMatches();
+    EndTurnPressed();
+    if(document.getElementById("lblTurn").innerText == player2Name + " Wins!")
+        DisplayResult(true,"UTEndTurnButtonWithComputerAndComputerWins");
+    else
+        DisplayResult(false,"UTEndTurnButtonWithComputerAndComputerWins");
 }
 function UTEndTurnButtonWithComputerAndPlayerWins(){
-    
+    isPlayerVsComputer = true;
+    isPlayer1Turn = true;
+    RemoveAllBut1Matches();
+    EndTurnPressed();
+    if(document.getElementById("lblTurn").innerText == player1Name + " Wins!")
+        DisplayResult(true,"UTEndTurnButtonWithComputerAndPlayerWins");
+    else
+        DisplayResult(false,"UTEndTurnButtonWithComputerAndPlayerWins");
 }
 function UTEndTurnButtonWithComputerAndNoOneWins(){
-    
+    isPlayerVsComputer = true;
+    isPlayer1Turn = true;
+    var beginMatchCount = GetMatchCount();
+    EndTurnPressed();
+    var endMatchCount = GetMatchCount();
+    if(endMatchCount < beginMatchCount)
+        DisplayResult(true,"UTEndTurnButtonWithComputerAndNoOneWins");
+    else
+        DisplayResult(false,"UTEndTurnButtonWithComputerAndNoOneWins");
 }
 function UTEndTurnButtonWithPlayerAndPlayer1Wins(){
-    
+    isPlayerVsComputer = false;
+    isPlayer1Turn = false;
+    RemoveAllBut1Matches();
+    EndTurnPressed();
+    if(document.getElementById("lblTurn").innerText == player1Name + " Wins!")
+        DisplayResult(true,"UTEndTurnButtonWithPlayerAndPlayer1Wins");
+    else
+        DisplayResult(false,"UTEndTurnButtonWithPlayerAndPlayer1Wins");
 }
 function UTEndTurnButtonWithPlayerAndPlayer2Wins(){
-    
+    isPlayerVsComputer = false;
+    isPlayer1Turn = true;
+    RemoveAllBut1Matches();
+    EndTurnPressed();
+    if(document.getElementById("lblTurn").innerText == player2Name + " Wins!")
+        DisplayResult(true,"UTEndTurnButtonWithPlayerAndPlayer2Wins");
+    else
+        DisplayResult(false,"UTEndTurnButtonWithPlayerAndPlayer2Wins");
 }
 function UTEndTurnButtonWithPlayerAndNoOneWins(){
-    
+    isPlayerVsComputer = false;
+    isPlayer1Turn = true;
+    var beginMatchCount = GetMatchCount();
+    document.getElementById("row2").removeChild(document.getElementById("row1").children[0]);
+    EndTurnPressed();
+    var endMatchCount = GetMatchCount();
+    if(endMatchCount < beginMatchCount)
+        DisplayResult(true,"UTEndTurnButtonWithPlayerAndNoOneWins");
+    else
+        DisplayResult(false,"UTEndTurnButtonWithPlayerAndNoOneWins");
 }
 function UTEndTurnButtonWithPlayerBeingSwitched(){
-    
+    isPlayerVsComputer = false;
+    isPlayer1Turn = true;
+    document.getElementById("lblTurn").innerText = player1Name + "'s Turn";
+    EndTurnPressed();
+    if(document.getElementById("lblTurn").innerText == player2Name + "'s Turn")
+        DisplayResult(true,"UTEndTurnButtonWithPlayerBeingSwitched");
+    else
+        DisplayResult(false,"UTEndTurnButtonWithPlayerBeingSwitched");
 }
 function UTGetMatchesWithValidRow(){
-
+    var row = GetMatches(2);
+    if(row.children.length > 1)
+        DisplayResult(true,"UTGetMatchesWithValidRow");
+    else
+        DisplayResult(false,"UTGetMatchesWithValidRow");
 }
 function UTGetMatchesWithInvalidRow(){
-    
+    RemoveAllMatches();
+    var row = GetMatches(1);
+    if(row.children.length == 1)
+        DisplayResult(true,"UTGetMatchesWithInvalidRow");
+    else
+        DisplayResult(false,"UTGetMatchesWithInvalidRow");
 }
 function UTComputerTurnOneChildRemoved(){
-    
+    isPlayerVsComputer = true;
+    isPlayer1Turn = false;
+    var beginMatchCount = GetMatchCount();
+    TakeComputerTurn();
+    var endMatchCount = GetMatchCount();
+    if(endMatchCount < beginMatchCount)
+        DisplayResult(true,"UTComputerTurnOneChildRemoved");
+    else
+        DisplayResult(false,"UTComputerTurnOneChildRemoved");
 }
 function UTComputerTurnDisabledButtons(){
+    isPlayerVsComputer = true;
+    isPlayer1Turn = false;
+    TakeComputerTurn();
+    var disabledCorrectly = document.getElementById('btnRow1').disabled &&
+                    document.getElementById('btnRow2').disabled;
     
+    if(document.getElementById('btnRow3')){
+        if(!document.getElementById('btnRow3').disabled) {
+            disabledCorrectly = false;
+        }
+    }
+    if(document.getElementById('btnRow4')){
+        if(!document.getElementById('btnRow4').disabled) {
+            disabledCorrectly = false;
+        }
+    }    
+    if(disabledCorrectly)
+        DisplayResult(true,"UTComputerTurnDisabledButtons");
+    else
+        DisplayResult(false,"UTComputerTurnDisabledButtons");
 }
 function UTFindRandomValidRowReturnsValidRow(){
-
+    var row = FindRandomValidRow();
+    if(row != undefined)
+        DisplayResult(true,"UTFindRandomValidRowReturnsValidRow");
+    else
+        DisplayResult(false,"UTFindRandomValidRowReturnsValidRow");
 }
 function UTCheckGameOverWithGameOver(){
-
+    RemoveAllMatches();
+    var isGameOver = CheckGameOver();
+    if(isGameOver)
+        DisplayResult(true,"UTCheckGameOverWithGameOver");
+    else
+        DisplayResult(false,"UTCheckGameOverWithGameOver");
 }
 function UTCheckGameOverWithNoGameOver(){
-
+    var isGameOver = CheckGameOver();
+    if(!isGameOver)
+        DisplayResult(true,"UTCheckGameOverWithNoGameOver");
+    else
+        DisplayResult(false,"UTCheckGameOverWithNoGameOver");
 }
 function DisplayResult(pass,functionName){
     if(pass)
         console.log("[PASSED] " + functionName);
     else
         console.log("[FAILED] " + functionName);
+}
+function RemoveAllBut1Matches(){
+    document.getElementById("row2").removeChild(document.getElementById("row1").children[0]);
+    document.getElementById("row2").removeChild(document.getElementById("row1").children[0]);
+    document.getElementById("row2").removeChild(document.getElementById("row1").children[0]);
+    document.getElementById("row3").removeChild(document.getElementById("row1").children[0]);
+    document.getElementById("row3").removeChild(document.getElementById("row1").children[0]);
+    document.getElementById("row3").removeChild(document.getElementById("row1").children[0]);
+    document.getElementById("row3").removeChild(document.getElementById("row1").children[0]);
+    document.getElementById("row3").removeChild(document.getElementById("row1").children[0]);
+}
+function RemoveAllMatches(){
+    document.getElementById("row1").removeChild(document.getElementById("row1").children[0]);
+    RemoveAllBut1Matches();
+}
+function GetMatchCount() {
+    return (document.getElementById("row1").children.length - 1) +
+            (document.getElementById("row2").children.length - 1) +
+            (document.getElementById("row3").children.length - 1) +
+            (document.getElementById("row4").children.length - 1);
 }
